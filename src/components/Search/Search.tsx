390a9e74f499dbaf925664d29ae83b83118074d4
bruceji7 <revolverc24@gmail.com>
2021-02-10 11:34:39 +0900

    Add: Timeout function for search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,29 @@
-import "./Search.css"
+import { useState, useEffect } from "react"
+
 import searchIcon from "../../style/images/search-icon.svg"
+import "./Search.css"
 
 export const Search = () => {
+  const [searchTerms, setSearchTerms] = useState<string>("")
+  const [result, setResult] = useState<string>("")
+
+  // Need a clear search button,
+  // A search successful button,
+  // A button that will let you go to what the search returned
+  // Also if the user presses enter, then they can go to that page - if the search is successful
+
+  // -- There will be a search result state and only if this contains something, a URL or something, then pressing enter will push that url.
+
+  useEffect(() => {
+    const searchTimer = setTimeout(() => {
+      setResult(searchTerms)
+    }, 700)
+
+    return () => {
+      clearTimeout(searchTimer)
+    }
+  })
+
   return (
     <div className="search">
       <div className="search-box">
@@ -9,8 +31,11 @@ export const Search = () => {
         <input
           className="search-input-field"
           placeholder="Search by block number or Transaction hash"
+          value={searchTerms}
+          onChange={(e) => setSearchTerms(e.target.value)}
         />
       </div>
+      <div>{result}</div>
     </div>
   )
 }
