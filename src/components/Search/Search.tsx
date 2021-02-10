b0e3edac60768116c219d51da36c0cb50ac34090
bruceji7 <revolverc24@gmail.com>
2021-02-10 12:14:10 +0900

    Fix: It now only searches once, until you change the string

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,16 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
+import axios from "../../axios/axiosinst"
 
 import searchIcon from "../../style/images/search-icon.svg"
 import "./Search.css"
 
 export const Search = () => {
   const [searchTerms, setSearchTerms] = useState<string>("")
+  const [hasSearched, setHasSearched] = useState<boolean>(false)
   const [result, setResult] = useState<string>("")
+  const [display, setDisplay] = useState<string>("")
+
+  const searchRef = useRef<HTMLInputElement | null>(null)
 
   // Need a clear search button,
   // A search successful button,
@@ -14,9 +19,23 @@ export const Search = () => {
 
   // -- There will be a search result state and only if this contains something, a URL or something, then pressing enter will push that url.
 
+  function handleSearchInput(str: string) {
+    setSearchTerms(str)
+    setHasSearched(false)
+  }
+
   useEffect(() => {
     const searchTimer = setTimeout(() => {
-      setResult(searchTerms)
+      // const currentSearch = searchRef.current?.value
+      if (hasSearched === false && searchTerms !== "") {
+        axios
+          .get(`/search/hash/${searchTerms}`)
+          .then((response) => {
+            setResult(response.data)
+          })
+          .catch((e) => console.error(e))
+      }
+      setHasSearched(true)
     }, 700)
 
     return () => {
@@ -24,18 +43,28 @@ export const Search = () => {
     }
   })
 
+  useEffect(() => {
+    if (result) {
+      //This function will be used to evaluate the results - if the search was successful, set result to a link
+      console.log("SEARCH: RESULT: ", result)
+    }
+  }, [result])
+
   return (
     <div className="search">
       <div className="search-box">
         <img className="search-icon" src={searchIcon} alt="" />
         <input
+          ref={searchRef}
           className="search-input-field"
           placeholder="Search by block number or Transaction hash"
           value={searchTerms}
-          onChange={(e) => setSearchTerms(e.target.value)}
+          onChange={(e) => {
+            handleSearchInput(e.target.value)
+          }}
         />
       </div>
-      <div>{result}</div>
+      <div></div>
     </div>
   )
 }
