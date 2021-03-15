9b92e6a099c6b5c84cae84fd62e1d70cc482d94d
bruceji7 <revolverc24@gmail.com>
2021-03-15 16:46:12 +0900

    Add: Redux Action to get contract txns

--- src/store/actions/contractActions.ts
+++ src/store/actions/contractActions.ts
@@ -1,12 +1,15 @@
 import axios from "../../axios/axiosinst"
 
-import { GET_ALL_CONTRACTS } from "./actionTypes"
+import { GET_ALL_CONTRACTS, GET_CONTRACT_TXNS } from "./actionTypes"
 
-import { ContractType } from "../../utility/types"
+import { ContractType, ObjectType } from "../../utility/types"
 
 //This function formats the action for the reducer to put into state
 const assembleAllContracts = (data: Array<ContractType>) => {
   return { type: GET_ALL_CONTRACTS, payload: { contractData: data } }
+}
+const assembleContractTxns = (data: Array<ObjectType>) => {
+  return { type: GET_CONTRACT_TXNS, payload: { contractTxns: data } }
 }
 
 // Here, hash is an argument given when this action is called.
@@ -17,5 +20,16 @@ export const getAllContracts = (channelHash: string) => {
       const contracts = response.data.chaincode
       dispatch(assembleAllContracts(contracts))
     })
+  }
+}
+export const getContractTxns = (channelHash: string, contractName: string) => {
+  return (dispatch: any) => {
+    axios
+      .get(`/chaincode/${channelHash}?chaincode=${contractName}`)
+      .then((response) => {
+        const contractTxns = response.data.row
+        dispatch(assembleContractTxns(contractTxns))
+        // look at txnActions.ts to see how to add from, if needed
+      })
   }
 }
--- src/store/reducers/contractReducer.ts
+++ src/store/reducers/contractReducer.ts
@@ -1,17 +1,25 @@
-import { GET_ALL_CONTRACTS } from "../actions/actionTypes"
+import { GET_ALL_CONTRACTS, GET_CONTRACT_TXNS } from "../actions/actionTypes"
 
-import { ContractType } from "../../utility/types"
+import { ContractType, ObjectType } from "../../utility/types"
 
 // In TS an action must be of a strict format. Set them here:
-type Action = {
-  type: typeof GET_ALL_CONTRACTS
-  payload: {
-    contractData: Array<ContractType>
-  }
-}
+type Action =
+  | {
+      type: typeof GET_ALL_CONTRACTS
+      payload: {
+        contractData: Array<ContractType>
+      }
+    }
+  | {
+      type: typeof GET_CONTRACT_TXNS
+      payload: {
+        contractTxns: Array<ObjectType>
+      }
+    }
 
 const initialState = {
   contractData: Array<ContractType>(),
+  contractTxns: Array<ObjectType>(),
 }
 
 const contractReducer = (state = initialState, action: Action) => {
@@ -23,6 +31,14 @@ const contractReducer = (state = initialState, action: Action) => {
       return {
         ...state,
         contractData: action.payload.contractData,
+      }
+    case GET_CONTRACT_TXNS:
+      if (!action.payload) {
+        return state
+      }
+      return {
+        ...state,
+        contractTxns: action.payload.contractTxns,
       }
     default:
       return {
