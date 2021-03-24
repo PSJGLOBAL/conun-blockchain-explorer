3543721dd3a076a61597c84b0034b75b52471c12
bruceji7 <revolverc24@gmail.com>
2021-03-24 17:08:41 +0900

    Add: Gist functionality and code tab

--- src/components/SmartContracts/ContractDetails/ContractCodeTab/ContractCodeTab.tsx
+++ src/components/SmartContracts/ContractDetails/ContractCodeTab/ContractCodeTab.tsx
@@ -1,7 +1,18 @@
+import Gist from "react-gist"
+
+import style from "./ContractCodeTab.module.css"
+
 const ContractCodeTab = () => {
   // Use GIST to get code, embed it in the page here.
 
-  return <div className="contract-code monofont">CODE HERE</div>
+  // const gistID = "ada1c0ce2b5e5c6b72943ef59cdaa7de"
+  const gistID = "64e1d17da3cc8ec2af55ba056d136603"
+
+  return (
+    <div className={style.block}>
+      <Gist id={gistID} />
+    </div>
+  )
 }
 
 export default ContractCodeTab
