7e923894b9fb8c7c8ba7453af8dbf8d625b19be7
bruceji7 <revolverc24@gmail.com>
2021-03-25 10:28:26 +0900

    Minor: Remove comment

--- src/components/SmartContracts/ContractDetails/ContractCodeTab/ContractCodeTab.tsx
+++ src/components/SmartContracts/ContractDetails/ContractCodeTab/ContractCodeTab.tsx
@@ -4,8 +4,6 @@ import style from "./ContractCodeTab.module.css"
 
 const ContractCodeTab = () => {
   // Use GIST to get code, embed it in the page here.
-
-  // const gistID = "ada1c0ce2b5e5c6b72943ef59cdaa7de"
   const gistID = "64e1d17da3cc8ec2af55ba056d136603"
 
   return (
